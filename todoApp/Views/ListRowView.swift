abf437adf0a0a602971d8388f2d35352c63c6eaa
jindongdu <djd28176@hotmail.com>
2023-12-09 08:25:34 -0500

    first

---
+++ todoApp/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  todoApp
+//
+//  Created by Jindong Du on 12/9/23.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
---
+++ todoApp/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  todoApp
+//
+//  Created by Jindong Du on 12/9/23.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
